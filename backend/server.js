d4e6811fdf8d3560ba39105c8ed5bbc50a70ccb0
Manjeet Saini <manjeet29saini@gmail.com>
2023-06-02 15:35:48 +0530

    adding basic server setup files

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import chalk from "chalk";
+import path from "path";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+
+// app configs goes here
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware goes here
+app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// --to accept data from Server
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+// All Routes Import
+
+// Api  EndPoints
+
+// --error Middlewares
+
+// Handling Uncaught Exception
+
+// DB Config
+
+// --------------------------deployment------------------------------
+
+// Listener
+app.listen(PORT, () =>
+  console.log(
+    `Server running in ${chalk.yellow.bold(
+      process.env.NODE_ENV
+    )} mode on port- ${chalk.blue.bold(PORT)}`
+  )
+);
+
+// Unhandled Promise Rejection
